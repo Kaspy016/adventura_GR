7bf246f8a14cacd93fb014c64a7174388c8ae365
Kaspy016 <kuba6kasparek@gmail.com>
2024-10-08 16:28:14 +0200

    Konfigurace Start

--- src/main/java/cz/vse/adventura/Start.java
+++ src/main/java/cz/vse/adventura/Start.java
@@ -7,16 +7,38 @@ package cz.vse.adventura;
 import cz.vse.adventura.logika.Hra;
 import cz.vse.adventura.logika.IHra;
 import cz.vse.adventura.uiText.TextoveRozhrani;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 
-public class Start
+public class Start extends Application
 {
 
     public static void main(String[] args)
     {
-        
-        IHra hra = new Hra();
-        TextoveRozhrani ui = new TextoveRozhrani(hra);
-        ui.hraj();
+        if (args.length>0 && args[0].equals("text")) {
+            IHra hra = new Hra();
+            TextoveRozhrani ui = new TextoveRozhrani(hra);
+            ui.hraj();
+        } else {
+            launch();
+        }
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("main.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        primaryStage.setTitle("Adventura");
     }
 }
