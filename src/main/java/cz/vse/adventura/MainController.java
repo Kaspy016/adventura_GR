3e5b0c85930b32f6647bed394604744f613085af
Kaspy016 <kuba6kasparek@gmail.com>
2024-10-10 10:05:52 +0200

    Focus na vstup
    Propojení se hrou, focus na vstup, výstup není editable

--- src/main/java/cz/vse/adventura/MainController.java
+++ src/main/java/cz/vse/adventura/MainController.java
@@ -1,5 +1,8 @@
 package cz.vse.adventura;
 
+import cz.vse.adventura.logika.Hra;
+import cz.vse.adventura.logika.IHra;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
@@ -7,13 +10,25 @@ import javafx.scene.control.TextField;
 
 public class MainController {
 
+    @FXML
     public TextArea vystup;
     @FXML
     private TextField vstup;
+    private IHra hra = new Hra();
+    @FXML
+    private void initialize() {
+        vystup.appendText(hra.vratUvitani()+"\n\n");
+        vystup.setEditable(false);
+        Platform.runLater(() -> vstup.requestFocus());
+        vstup.requestFocus();
+    }
 
     @FXML
-    public void odesliVstup(ActionEvent actionEvent) {
-        vystup.appendText(vstup.getText() + "\n");
+    private void odesliVstup(ActionEvent actionEvent) {
+        String prikaz = vstup.getText();
+        vystup.appendText("> "+prikaz+"\n");
+        String vysledek = hra.zpracujPrikaz(prikaz);
+        vystup.appendText(vysledek+"\n\n");
         vstup.clear();
     }
 }
